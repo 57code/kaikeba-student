8c8c32421e42da05253cd5376edcf55f19856e8a
yt0379 <yt0379@qq.com>
2018-11-21 22:51:42 +0800

    TypeScript初始

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,61 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+// 变量、常量
+let name = 'tom';
+const PI = 3.1415926;
+let foo: string; // 声明变量类型
+foo = 'foo'; // ok
+let isDone: boolean; // 声明布尔型
+isDone = true; // ok
+name = 'jerry';
+
+const names: string[] = ['a', 'b'];
+
+// 元组
+let x: [string, number];
+x = ['hello', 10]; // ok
+
+// 任意类型any
+let notSure: any;
+notSure = 4;
+notSure = 'aaaa';
+
+// any也能用于数组
+const list: any[] = [1, true, 'aaa'];
+list[1] = 100;
+
+// 枚举
+enum Color {Red = 1, Green = 2, Blue = 3}
+
+const c: Color = Color.Blue;
+console.log(c); // 3
+console.log(Color[1]); // Red
+
+
+// 函数中使用类型约束
+function greeting(person: string): string {
+  return 'Hello, ' + person;
+}
+
+greeting('tom');
+
+// void类型
+function warnUser(): void {
+  alert('aaaaaaaa');
+}
+
+// 接口
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeting2(person: Person) {
+  return 'hello, ' + person.firstName + ' ' + person.lastName;
+}
+
+const myname = greeting2({firstName: 'tom', lastName: 'cruise'});
+console.log(myname);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +63,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'angular';
 }
